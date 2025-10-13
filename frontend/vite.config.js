8e6aba7c7b07ec634d8e30eb852495611a193926
Adrián Villanueva <16082421+adrianvillanueva997@users.noreply.github.com>
2025-10-13 20:01:33 +0900

    perf: Vite plugins (#8)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,18 @@
 import yaml from '@rollup/plugin-yaml'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { robots } from 'vite-plugin-robots'
+import Sitemap from 'vite-plugin-sitemap'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
     plugins: [
         vue(),
-        yaml()
+        yaml(),
+        ViteImageOptimizer(),
+        robots(),
+        Sitemap({ hostname: 'https://resume.adrianvillanueva.com' })
     ],
     css: {
         preprocessorOptions: {
